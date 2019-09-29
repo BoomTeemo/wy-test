70d90cee101068d311620c2ff23393aced53d118
BoomTeemo <1157878136@qq.com>
2019-09-29 20:27:20 +0800

    TinyURL是一种URL简化服务， 比如：当你输入一个URL https://leetcode.com/problems/design-tinyurl 时，它将返回一个简化的URL http://tinyurl.com/4e9iAk.
    要求：设计一个 TinyURL 的加密 encode 和解密 decode 的方法。你的加密和解密算法如何设计和运作是没有限制的，你只需要保证一个URL可以被加密成一个TinyURL，并且这个TinyURL可以用解密方法恢复成原本的URL。

---
+++ src/leetcode/TinyURLDeJiaMi.java
@@ -0,0 +1,19 @@
+package leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class TinyURLDeJiaMi {
+    Map<Integer, String> map = new HashMap<>();
+    int i = 0;
+
+    public String encode(String longUrl) {
+        map.put(i, longUrl);
+        return "http://tinyurl.com/" + i++;
+    }
+
+    public String decode(String shortUrl) {
+        return map.get(Integer.parseInt(shortUrl.replace("http://tinyurl.com/", "")));
+    }
+
+}
